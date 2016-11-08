3a35e162873684f6e7d3e374e4c825643a6e50a1
Raphael Vignes <rvignes@c133-14.enst.fr>
2016-11-08 10:37:41 +0100

    Maj javadoc ReduceThread

--- MASTER SHAVADOOP JAR/src/MapThread.java
+++ MASTER SHAVADOOP JAR/src/MapThread.java
@@ -33,7 +33,7 @@ public class MapThread extends Thread {
 	/**
 	 * Overriden run method of Thread 
 	 * Starts the MapThread which remotely calls the Main of the Slave jar file.
-	 * This call is done by a bash command line, which invoke the command java -jar with the path to the jar and 
+	 * This call is done by a bash command line, which invoke the command java -jar with the path to the jar map option and 
 	 * the index number of the Sx file on given host by ssh @see SSHUtils, Main of SLAVE SHAVADOOP JAR
 	 */
 	public void run() {
