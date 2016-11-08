dfa0d4dfb50a2ca7124e21245bc03eb126277add
Raphael Vignes <rvignes@c133-14.enst.fr>
2016-11-08 09:23:35 +0100

    Maj javadoc Mapper

--- MASTER SHAVADOOP JAR/src/Mapper.java
+++ MASTER SHAVADOOP JAR/src/Mapper.java
@@ -7,9 +7,14 @@ import java.util.Map;
 
 
 public class Mapper {
+	/**
+	 * Class instantiated by the main of MASTER SHAVADOOP JAR with the list of available hosts and all the lines from the input text
+	 * 
+	 * 
+	 */
 
 	static final String W = "/cal/homes/aroville/";
-	static final int MAX_WORK_LOAD = 20;
+	static final int MAX_WORK_LOAD = 20;//Max MapThreads to start
 
 	List<String> hosts;
 	List<String> lines;
@@ -17,6 +22,11 @@ public class Mapper {
 	Map<String, ArrayList<Integer>> keyUMx;
 
 	/**
+	 * The constructor of the Mapper wich initialise the attributes hosts with the list provided by the Main,
+	 * the attribute lines with the list of lines provided by the Main.
+	 * It also initialise the attribute threads with a new list of mappers @see MapThread class
+	 * and the attribute keyUMx with a new Map of String,Arraylist
+	 * @param hosts, lines
 	 */
 	Mapper(List<String> hosts, List<String> lines) {
 		this.hosts = hosts;
@@ -26,7 +36,11 @@ public class Mapper {
 	}
 
 	/**
-	 * 
+	 * This method distributes the data (one or several lines depending on the configuration of a split) on the available Hosts.
+	 * It proceeds by instanciating MapThreads, @see MapThread, giving the host on which we want the computation to be done
+	 * and the index of the Sx file to give to the MapThread
+	 * The distribution is done using a modulo between the number of splits and the number of available hosts
+	 *  
 	 */
 	void split() {
 		System.out.println("Start mapping");
@@ -54,22 +68,27 @@ public class Mapper {
 			e.printStackTrace();
 		}
 
-		System.out.println("Splits over");
+		System.out.println("Distribution over");
 	}
-
+	/**Enqueues a given MapThread and calls its start method 
+	 * @param t
+	 */
 	synchronized void queue(MapThread t) {
 		threads.add(t);
 		t.start();
 	}
 
-	synchronized void dequeue(MapThread t) {
+	/**Dequeues a given MapThread and calls the method storeKeyUMx 
+	 * @param t
+	 */
+	synchronized void dequeue(MapThread t)
+	{
 		threads.remove(t);
 		storeKeyUMx(t);
 	}
 
-
 	/**
-	 * Retrieve the collected keys and the associated file
+	 * Retrieve the collected keys and the associated UMx file
 	 * @param t
 	 */
 	private void storeKeyUMx(MapThread t) {
@@ -83,7 +102,12 @@ public class Mapper {
 		}
 	}
 
-
+/**
+ * Writes the split i into a a Sx file
+ * @param i
+ * @return true if the line i is correctly written to the Sx file (file needed in input of the slave) 
+ * or false if the line is empty or if any exception
+ */
 	private boolean writeLine(Integer i) {
 		try {
 			String s = clean(lines.get(i)).trim();
@@ -97,21 +121,36 @@ public class Mapper {
 		} catch (Exception e) {
 			return false;
 		}
-		
-		
+
 	}
 
-	// GETTER
+	/**
+	 * Getter of keyUMx 
+	 * 
+	 * @return keyUMx
+	 */
 	Map<String, ArrayList<Integer>> getKeyUMx() {
 		return keyUMx;
 	}
 	
+	
+	
+	/**Restarts a given MapThread if its instanciation failed. 
+	 * As we are network dependent (MapThread are started on other machines by SSH)
+	 * we can encounter issues when trying to connect.
+	 * @see MapThread
+	 * @param t
+	 */
 	void retry(MapThread t) {
 		System.out.println("Retrying for idx = " + t.getIdx());
 		threads.remove(t);
 		queue(new MapThread(t.getHost(), t.getIdx(), this));		
 	}
 	
+	/**Cleans a given line by removing all non alphanumeric characters
+	 * @return cleaned line s
+	 * @param s 
+	 */
 	String clean(String s) {
 		return s.replaceAll("[^a-zA-Z0-9]+"," ");
 	}
